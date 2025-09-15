0a15820fc38a3232cbe2477a150771746c75fbf2
이채빈 <dlcoqls002@gmail.com>
2025-09-15 18:07:04 +0900

    테스트

---
+++ Run/Assets/Prefab/Scripts/EnemyMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public Animator animator;
+    public float moveSpeed = 3f;
+    private bool isMoving = true;
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            // 적 캐릭터의 정면 방향으로 계속 이동
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+            animator.SetInteger("animation", 18);
+        }
+        else
+        {
+            animator.SetInteger("animation", 34);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            isMoving = false; // 이동 멈추기
+            Debug.Log("Enemy reached the goal and stopped.");
+        }
+    }
+}
