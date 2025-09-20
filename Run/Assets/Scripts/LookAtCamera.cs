215804a24ee7b32c7478fd9f01fa79f6638ad6ab
이채빈 <dlcoqls002@gmail.com>
2025-09-20 21:09:46 +0900

    적 따라다니는 체력바 갱신

---
+++ Run/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (mainCam != null)
+        {
+            // 체력바가 항상 카메라를 정면으로 바라보게
+            transform.forward = mainCam.transform.forward;
+        }
+    }
+}
